c322946ec9378d07c66f7c25da936b2c4c33086f
Joseph Jordan <jumpman.fam314@gmail.com>
2020-06-24 10:58:57 -0500

    saving place

--- components/views/home.js
+++ components/views/home.js
@@ -1,12 +1,8 @@
 export default () => `
   <section id="jumbotron">
-  <div class="container">
-  <div class="jumbotron">
     <h1>Food is Medicine</h1>
     <p>Learn how to let food be thy medicine.</p>
-  </div>
-  <p>This is some text about some testimonies.</p>
-  <p>This is some more text about more testimonies.</p>
-  </div>
+    <p>This is some text about some testimonies.</p>
+    <p>This is some more text about more testimonies.</p>
   </section>
 `;
