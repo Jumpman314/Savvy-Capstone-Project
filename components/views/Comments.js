7f7d1e5a7dff3c267e956952ca0e2a12ab2aaec8
Joseph Jordan <jumpman.fam314@gmail.com>
2020-06-24 16:00:27 -0500

    saving place

--- components/views/Comments.js
+++ components/views/Comments.js
@@ -2,7 +2,6 @@ export default () => `
   <section id="comments">
   <h1>Share Your Story</h1>
   <form id="fs-frm" name="simple-contact-form" accept-charset="utf-8" action="www.google.com/jumpman.fam314@gmail.com" method="post">
-  <fieldset id="fs-frm-inputs">
     <label for="full-name">Full Name</label>
     <input type="text" name="name" id="full-name" placeholder="First and Last" required="">
     <label for="email-address">Email Address</label>
@@ -10,7 +9,6 @@ export default () => `
     <label for="message">Message</label>
     <textarea rows="5" name="message" id="message" placeholder="Comment Here." required=""></textarea>
     <input type="hidden" name="_subject" id="email-subject" value="Contact Form Submission">
-  </fieldset>
   <input type="submit" value="Submit">
 </form>
   </section>
